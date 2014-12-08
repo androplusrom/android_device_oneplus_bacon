139666ead70f5b7f6c4eeff5f67eb7c1c896c065
nickdoherty81 <nickdoh81@gmail.com>
2014-12-08 04:13:22 -0500

    Assify boardconfig and new aosp boot Thanks to guerreromanuel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,9 @@
 TARGET_KERNEL_CONFIG := cyanogenmod_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 
+#ASSIFY
+ASSIFY := true
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
 
